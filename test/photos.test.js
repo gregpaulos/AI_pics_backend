661a464b47ac44f86827bdeb69dfe5a6c4db757c
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-03-19 15:54:10 -0600

    added tests for photo routes

--- test/photos.test.js
+++ test/photos.test.js
@@ -3,16 +3,17 @@ const request = require("supertest");
 const app = require("../index");
 const database = require("../db_connection");
 
-var url=""
 
 describe("Photos route", () => {
     describe("#photo route index", () => {
-        it("it up and runs", done => {
+        it("it returns a list of all photos", done => {
             request(app)
                 .get("/v1/photos")
                 .expect(200)
                 .then(response => {
-                    assert.deepEqual(response.body, "photos route says hello!");
+                    let results = response.body.results
+                    assert.include(results[0].photo_url, "https://images.unsplash.com/photo-1518843025960-d60217f226f5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE5NjkzfQ&s=8a28efad39094a7be3a369396332fd02");
+                    assert.isAtLeast(results.length, 1);
                 }).then(done).catch(done);
         });
     });
@@ -26,32 +27,40 @@ describe("Photos route", () => {
                 }).then(done).catch(done)
         });
     });
-    // rate-limited, so hit with care
-    describe("#gets a new photo, sends it to AI and gets result", () => {
-        it("unsplash returns an image url", done => {
+    describe("#photo id with description route", () => {
+        it("returns image from db by id with the ai descriptions", done => {
             request(app)
-                .get("/v1/photos/random")
+                .get("/v1/photos/1/descriptions")
                 .expect(200)
                 .then(response => {
-                    url = response.body
-                    assert.include(response.body, 'https://images.unsplash.com/photo');
+                    assert.equal(response.body.photo_and_description.google[0], "fell")
                 }).then(done).catch(done)
         });
-        it("sends to clarifai", done => {
-            console.log('THIS IS THE URL', url);
-            let body = {}
-            body.photo = url;
-            
-            let JSONbodyToPost = JSON.stringify(body) 
-            console.log('ABOUT TO POST', JSONbodyToPost);
+    });
+    describe("#descriptions", () => {
+        it("returns all descriptions from db", done => {
             request(app)
-                .post("/v1/ai/clarifai")
-                .send(JSONbodyToPost) 
+                .get("/v1/photos/descriptions")
                 .expect(200)
                 .then(response => {
-                    assert.include(response.body.results[0].photo_url, "https://images.unsplash.com/photo-1518843025960-d60217f226f5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE5NjkzfQ&s=8a28efad39094a7be3a369396332fd02")
+                    let results = response.body.results
+                    assert.include(results[0].description, "mountainous landforms");
+                    assert.isAtLeast(results.length, 5);
+ 
                 }).then(done).catch(done)
         });
-    }); 
+    });
+    describe("#descriptions specific ai provider", () => {
+        it("returns all descriptions from db for a particular ai provider", done => {
+            request(app)
+                .get("/v1/photos/descriptions/google")
+                .expect(200)
+                .then(response => {
+                    let results = response.body["photos_and_descriptions"]
+                    assert.include(results["1"].google[0], "fell");
+                }).then(done).catch(done)
+        });
+    });
+ 
 });
 
